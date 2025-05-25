74cf0a041ff34673fecbf6b0ffe5ddf2b864f4dc
Stefan <stefangrzelec@gmail.com>
2025-05-25 15:27:58 +0200

    dotenv

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+import os
 from app import create_app
+#dotenv
+from dotenv import load_dotenv
+load_dotenv()
 
 app = create_app()
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(port=port, debug=(os.environ.get("DEBUG", "False").lower() == "true"))
