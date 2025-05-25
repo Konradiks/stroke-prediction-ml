5c553a2f37c52a3b2b5ad4473ec067e46623338c
Stefan <stefangrzelec@gmail.com>
2025-05-25 16:16:45 +0200

    cors

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 import os
 from app import create_app
+from flask_cors import CORS
 #dotenv
 from dotenv import load_dotenv
 load_dotenv()
 
 app = create_app()
+
+CORS(app)
 
 
 if __name__ == "__main__":
